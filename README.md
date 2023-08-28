6f6eeb38158dd93acc1aa4d13e7203433e545c34
anoush <dakeon@outlook.fr>
2023-08-29 00:00:46 +0200

    new PATH argument added

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ downloads (atm only a select amount) all the pins images from a given pinterest 
 
 ## usage
 
-`./pinterest-image-scraper URL`
+`./pinterest-image-scraper URL PATH`
 
 where `URL` is your pinterest board url.
-the program will then create a `/images` directory with all the pins images downloaded as jpg files.
+the program will then create a `images` (or `PATH` if given as argument) directory with all the pins images downloaded as `.jpg` files.
+
+`PATH` is the name of the folder your photos will be downloaded in. it is **optional** and as said above the default name is `images`.
--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,16 @@ use scraper::{Html, Selector};
 use std::fs;
 
 /// saves imave from url to path
-fn save_image(url: &String, path: &String) -> () {
-    let img_bytes = reqwest::blocking::get(url).unwrap().bytes().unwrap();
-    let mut file = File::create(path).expect("file note created");
+fn save_image(url: &String, path: &String) -> () 
+{
+    let img_bytes = reqwest::blocking::get(url).expect("cannot load pin image").bytes().expect("cannot convert pin image to bytes");
+    let mut file: File = File::create(path).expect("file note created");
     file.write(&img_bytes).expect("not written");
-
 }
 
 
-fn main() {
-
+fn main() 
+{
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Usage: {} <URL>", args[0]);
@@ -23,20 +23,26 @@ fn main() {
     }
     let url: &String = &args[1];
 
-    fs::create_dir_all("images").expect("cannot create directory");
+    let path_directory: &str = if args.len() == 3 { &args[2] } else {"images"};
 
-    let response = reqwest::blocking::get(url).unwrap().text().unwrap();
 
-    let page = Html::parse_document(&response);
+    // creates the image directory
+    fs::create_dir_all(path_directory).expect("cannot create directory");
+
+    // loads (gets) pinterest url
+    let response: String = reqwest::blocking::get(url).expect("cannot load pinterest url").text().unwrap();
+    // parses the result
+    let page: Html = Html::parse_document(&response);
 
     let selector = Selector::parse("img").unwrap();
-    let selector1 = Selector::parse("srcset").unwrap();
+    //let selector1 = Selector::parse("srcset").unwrap();
     let mut c = 1;
 
     for element in page.select(&selector) {
         match element.value().attr("src") {
             Some(e) => {//println!("{}", e)
-                let path = "images/image".to_owned() + &c.to_string() + ".jpg";
+                // creates the image name
+                let path: String = path_directory.to_owned() + "/image" + &c.to_string() + ".jpg";
                 save_image(&e.to_owned(), &path);
                 c += 1;
             },
